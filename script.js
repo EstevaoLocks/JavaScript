b073780550ddd88d221e082d8ae0bd60cb5c524e
EstevaoLocks <estevao.locks11@gmail.com>
2025-08-21 11:09:44 -0300

    salvando

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 let conteudo = document.getElementById("conteudo");
 let btn = document.getElementById("btn");
-
+let flag = true;
+let tagp = '';
 
 btn.addEventListener("click", function () {
-    if (conteudo.parentNode) {
+    if (conteudo.parentNode && flag) {
         conteudo.parentNode.removeChild(conteudo);
 
         document.getElementById("btn").innerHTML = "Add";
+        flag = false;
+    }else {
+        tagp = document.querySelector("body").createElement('p');
+        tagp.setAttribute('id', 'conteudo');
+        tagp.innerHTML = "conteudo a remover";
+
+        document.getElementById("btn").innerHTML = "Deletar";
+        flag = true;
     }
 });
+
