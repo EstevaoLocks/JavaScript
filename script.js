18b9ae0fb2e73520d7b6fd496a93952b09569ff1
EstevaoLocks <estevao.locks11@gmail.com>
2025-08-21 07:41:23 -0300

    sincronizando

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let conteudo = document.getElementById("conteudo");
 let btn = document.getElementById("btn");
 
+
 btn.addEventListener("click", function () {
     if (conteudo.parentNode) {
         conteudo.parentNode.removeChild(conteudo);
