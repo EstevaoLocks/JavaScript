947af18128e6c7d23d0ba25a2b3279d5f9938642
EstevaoLocks <estevao.locks11@gmail.com>
2025-08-28 09:38:59 -0300

    sincronizando

---
+++ Set-Backcolor/script.js
@@ -0,0 +1,8 @@
+//Defino o botão
+let btn = document.getElementById("btn");
+//Define bandeira sinalizadora
+let flag = true;
+
+btn.addEventListener("click", function () {
+    document.body.style.backgroundColor = ran
+});
