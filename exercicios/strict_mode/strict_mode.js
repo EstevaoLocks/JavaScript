8ec0f2cf47c16e35badbe2ff07c045fd73c1b377
EstevaoLocks <estevao.locks11@gmail.com>
2026-02-02 16:00:25 -0300

    about strict mode

---
+++ exercicios/strict_mode/strict_mode.js
@@ -0,0 +1,16 @@
+
+"use strict";
+
+// Gera um erro 
+/*
+otherName = "Fulano de Tal";
+console.log(otherName);
+console.log("Nome -> Fulano de Tal");
+console.log("Nome: " + otherName);
+*/
+
+// Não gera um erro
+let otherName = "Fulano de Tal";
+console.log(otherName);
+console.log("Nome -> Fulano de Tal");
+console.log("Nome: " + otherName);
